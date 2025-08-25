bbadbfff8b51d4b1c981f7d99cb6249898e32ee5
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2025-08-25 16:27:23 -0400

    fixed light/dark toggle button

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         {/* Avoid flash of wrong theme */}
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
